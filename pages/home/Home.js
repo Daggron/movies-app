bed2c1a646065fe68f2848dda145b69cc1af9b66
Daggron <returnofking04@gmail.com>
2020-01-18 23:52:01 +0530

    Improved the ui

--- pages/home/Home.js
+++ pages/home/Home.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import {View , Text , FlatList, Image, StyleSheet ,  ScrollView } from 'react-native';
 import { TouchableOpacity} from 'react-native-gesture-handler';
-import Joker from './poster/joker.jpg'
+import Joker from './poster/joker.jpg';
+import KGF from './poster/kgf.jpg';
+import Inception from './poster/inception.jpg'
+import Shutter from './poster/shutter.jpg';
+import John from './poster/johnwick.jpg';
+import Endgame from './poster/endgame.jpg';
 
 export default function Home() {
     const [movie , setMovie] = React.useState([
@@ -12,29 +17,54 @@ export default function Home() {
         },
         {
             title : "K.G.F",
-            image : Joker,
+            image : KGF,
             id : 2
         },
         {
-            title : "Joker",
-            image : Joker,
+            title : "Inception",
+            image : Inception,
             id : 3
         },
         {
-            title : "K.G.F",
-            image : Joker,
+            title : "John Wick",
+            image : John,
             id : 4
+        },
+        {
+            title : "Shutter Island",
+            image : Shutter,
+            id : 5
+        },
+        {
+            title : "Avengers : Endgame",
+            image : Endgame,
+            id : 6
         }
 
     ])
     return (
         <ScrollView>
-        <View style={style.Container}>
+       
+        <View>
             
-            <Text>
-                Recomendations
+            <Text style={{flex : 1 , fontSize : 20 , justifyContent : "center" , alignContent : "center" , alignItems : "center" , flexWrap : "wrap" , textAlign: "center" , marginTop : 20}}>
+                Discover
             </Text>
-            <Image source={Joker} style={{width : 300 , height : 200 , borderRadius : 20 , marginTop : 200}}/>
+            <View style={style.Container}>
+            <ScrollView 
+                horizontal
+                pagingEnabled
+                showsHorizontalScrollIndicator={false}
+            >
+            <Image source={Joker} style={{width : 300 , height : 200 , borderRadius : 20 , marginTop : 50}}/>
+            <Text style={style.text}>
+                Joker
+            </Text>
+            </ScrollView>
+            </View>
+            <Text style={{position : "relative" ,top : 0 , left : 0  ,marginTop : 15 , fontSize : 20 , flex : 1 , justifyContent: "flex-start" , flexWrap : "wrap"  , alignContent : "flex-start" , alignItems: "flex-start" , margin : 15 }}>
+                Top Picks
+            </Text>
             <TouchableOpacity>
                 <FlatList 
                 showsHorizontalScrollIndicator={false}
@@ -66,5 +96,13 @@ const style = StyleSheet.create({
        alignContent : "flex-end",
        alignItems : "center",
        alignSelf : "center"
+    },
+    text:{
+        backgroundColor : "white",
+        padding : 20,
+        marginTop : -30,
+        fontSize : 15,
+        width : 150,
+        borderRadius : 10
     }
 })
