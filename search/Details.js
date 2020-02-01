acd72cd20e7fd3f143f934c2d48fcdf5e726e5a2
Daggron <returnofking04@gmail.com>
2020-02-02 03:11:50 +0530

    Improved ui a bit

--- search/Details.js
+++ search/Details.js
@@ -7,9 +7,10 @@ function Details(props){
 
     const [movie,setMovie] = React.useState({});
     const [Rating , setRating] = React.useState([]);
+    const [title , setTitle] = React.useState(props.navigation.getParam('title'));
 
     React.useEffect(()=>{
-        const title = props.navigation.getParam('title');
+        // const title = props.navigation.getParam('title');
         console.log(title);
         Axios.get(`http://www.omdbapi.com/?apikey=19a92861&t=${title}&plot=full`)
         .then(res=>{
@@ -21,7 +22,12 @@ function Details(props){
             alert('No Data Found')
             console.log(err);
         })
-    },[])
+    },[title])
+
+    if(props.navigation.getParam('title')!==title){
+        let data = props.navigation.getParam('title')
+        setTitle(data)
+    }
     
     if(movie){
         return(
@@ -31,27 +37,32 @@ function Details(props){
             >
                 <View style={{backgroundColor : "#f5f5f5" }}>
                     <Image source={{uri : movie.Poster}} resizeMode="cover" blurRadius={1} style={{ width : Dimensions.get('window').width , height : 300}}/>
-                    <View style={{ padding : 20 , backgroundColor : "#f5f5f5" , marginTop : -50 , borderTopLeftRadius : 50 , borderTopRightRadius : 50 , width : Dimensions.get('window').width}}>
-                        <Text style={{fontSize: 20 , textAlign :"center" }}>
+                    <View style={{ flex : 1 , justifyContent : "space-between", alignContent : "center", flexDirection : "row"  , flexWrap : "wrap" , padding : 20 , backgroundColor : "#f5f5f5" , marginTop : -50 , borderTopLeftRadius : 50 , borderTopRightRadius : 50 , width : Dimensions.get('window').width}}>
+                        
+                        <Image source={{uri : movie.Poster}} resizeMode="contain" style={{height : 200 , width :110}}/>
+                        
+                        <View style={{marginTop : 30}}>
+                        <Text style={{ textAlign :"center" , fontSize : 15 , width : 200 }}>
                             {
                                 movie.Title
                             }
                         </Text>
-                        <Text style={{fontSize : 12 , textAlign : "center" , marginTop : 15}}>
+                        <Text style={{fontSize : 12 , textAlign : "center" ,  width : 200  , marginTop : 15}}>
                             {
                                 movie.Genre
                             }
                         </Text>
-                        <Text style={{fontSize : 12 , textAlign : "center" , marginTop : 15}}>
+                        <Text style={{fontSize : 12 , textAlign : "center" , width : 200  , marginTop : 15}}>
                             Director: {
                                 movie.Director
                             }
                         </Text>
-                        <Text style={{fontSize : 12 , textAlign : "center" , marginTop : 15}}>
+                        <Text style={{fontSize : 12 , textAlign : "center" ,  width : 200 , marginTop : 15}}>
                            Rated: {
                                 movie.Rated
                             }
                         </Text>
+                        </View>
                     </View>
                     <View>
                         <Text style={{fontSize : 20 , marginTop : 20  , paddingHorizontal : 20}}>
